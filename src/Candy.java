04eac6ac5b16549bbfb2d012b6eedc56f85af59c
feal24 <feal24te@student.ju.se>
2024-11-12 15:38:50 +0100

    created a candy class

---
+++ src/Candy.java
@@ -0,0 +1,31 @@
+public class Candy {
+    String taste;
+    int numberOf;
+
+    public Candy(String taste, int numberOf) {
+        this.taste = taste;
+        this.numberOf = numberOf;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public int getNumberOf() {
+        return numberOf;
+    }
+
+    public void setNumberOf(int numberOf) {
+        this.numberOf = numberOf;
+    }
+
+    @Override
+    public String toString() {
+        return "Smak: " + taste + ", Antal: " + numberOf;
+    }
+
+}
