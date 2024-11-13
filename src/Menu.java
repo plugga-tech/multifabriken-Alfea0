713077dd52a139dfdd7d46f89109095ee28bf1d3
feal24 <feal24te@student.ju.se>
2024-11-13 16:06:01 +0100

    created choice menu

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+    public static void showMenu() {
+        System.out.println("~~ Multifabriken AB ~~");
+        System.out.println("");
+        System.out.println("1. Lägg till en ny bil");
+        System.out.println("2. Lägg till en ny godis");
+        System.out.println("3. Lägg till ett nytt rör");
+        System.out.println("4. Lägg till en ny havremjölk");
+        System.out.println("5. Visa beställningen");
+        System.out.println("6. Avsluta");
+        System.out.println("");
+        System.out.println("Välj ett alternativ: ");
+    }
+}
